ed9211d3e91e216f0a37662d25687e517992fedf
San Pedro <84334698+pstepanov-crypto@users.noreply.github.com>
2023-04-10 16:45:56 +0000

    Update parse.py

--- parseRegular/parse.py
+++ parseRegular/parse.py
@@ -1,1 +1,27 @@
+import csv
+from pprint import pprint
+import yaml
+import re
+from netmiko import (
+    ConnectHandler,
+    NetmikoTimeoutException,
+    NetmikoAuthenticationException,
+)
 
+
+def send_show_command(device, commands):
+    with ConnectHandler(**device) as ssh:
+        ssh.enable()
+        result = ssh.send_config_set(commands)
+    return result
+    
+
+if __name__ == "__main__":
+    with open("devices.yaml") as f, open("write.csv", "w") as s:
+        devices = yaml.safe_load(f)
+        writer = csv.writer(s) #a variable for writing in csv
+        for device in devices:
+          result = send_show_command(device, ["sh arp"])
+          match = re.findall(r"Internet  (\S+)", result) #regular expression can change 
+          writer.writerow([device['host'],[match]]) # parse and writing in csv file 
+        print(match)
