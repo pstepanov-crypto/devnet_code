641c53d1d4e922036ba11810877d2e63420e2892
San Pedro <84334698+pstepanov-crypto@users.noreply.github.com>
2023-08-21 17:31:49 +0300

    Create csv_to_yml.py

---
+++ csv_to_yml_format/csv_to_yml.py
@@ -0,0 +1,38 @@
+import csv
+import yaml
+
+excel_filename = "IP-addresses_172.csv"
+yaml_filename = excel_filename.replace('csv', 'yaml')
+
+common_name = "generate prefix"
+netbox = ""
+url = '"ansible_ip_addess"'
+token = "apy key netbox"
+data = ""
+state = "present"
+
+users = []
+
+with open(excel_filename, "r") as excel_csv:
+    csv_reader = csv.reader(excel_csv)
+    for line in csv_reader:
+        if len(line) == 4:
+            opis, prefix, descr, coma = [item.strip() for item in line]
+            full_prefix = f"{prefix}/{descr}"
+              
+            user_data = {
+                '- name': common_name,
+                '  netbox.netbox.netbox_prefix': netbox,
+                '    netbox_url': url,
+                '    netbox_token': token,
+                '    data': f"\n          prefix: {full_prefix}\n          description: {opis}\n          comments: {coma}\n        state: {state}\n"
+            }
+            users.append(user_data)
+
+
+
+with open(yaml_filename, "w+") as yf:
+    yf.write("  tasks:\n")
+    for user in users:
+        for k, v in user.items():
+            yf.write(f"    {k}: {v}\n")
