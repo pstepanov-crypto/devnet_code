ca0c8b667956d772e7e75bd98a207c51ccdc865a
San Pedro <84334698+pstepanov-crypto@users.noreply.github.com>
2024-03-21 13:34:39 +0300

    Update sfp_parse.py

--- sfp_parse/sfp_parse.py
+++ sfp_parse/sfp_parse.py
@@ -1,12 +1,13 @@
 #The script is parse sfp based on regular expression and used command - show inventory | include SFP | SN
+# Add network ranges
 from netmiko import ConnectHandler
 import ipaddress
 import re
 import csv
 from paramiko.ssh_exception import SSHException
 
-# Define the network range
-network = ipaddress.ip_network('192.168.0.0/24')
+# Define the network ranges
+network_ranges = ['192.168.0.0/24', '192.168.1.0/24']
 
 # Define the device parameters
 device_params = {
@@ -15,35 +16,39 @@ device_params = {
     'password': 'cisco'
 }
 
-# Loop through each IP address in the network range
-for ip in network:
-    device_params['ip'] = str(ip)
-    try:
-        # Connect to the device
-        net_connect = ConnectHandler(**device_params)
+# Loop through each network range
+for network_range in network_ranges:
+    network = ipaddress.ip_network(network_range)
 
-        # Send the command to get sfp information
-        output = net_connect.send_command('show inventory | include SFP | SN')
+    # Loop through each IP address in the network range
+    for ip in network:
+        device_params['ip'] = str(ip)
+        try:
+            # Connect to the device
+            net_connect = ConnectHandler(**device_params)
 
-        # Regular expression to extract SFP information
-        match = re.findall(r'(SFP-\S+) (?:\s*,\s*VID:\s*[^,]+,\s*) SN:(\s*[^\s]+)', output)
+            # Send the command to get sfp information
+            output = net_connect.send_command('show inventory | include SFP | SN')
 
-        # Determine the hostmame
-        hostname = device_params['ip']
+            # Regular expression to extract SFP information
+            match = re.findall(r'(SFP-\S+) (?:\s*,\s*VID:\s*[^,]+,\s*) SN:(\s*[^\s]+)', output)
 
-        # Write to CSV
-        with open("sfp.csv", mode="a", newline="") as file:
-            writer = csv.writer(file)
-            for sfp_info in match:
-                writer.writerow([hostname, sfp_info[0], sfp_info[1]])
+            # Determine the hostname
+            hostname = device_params['ip']
 
-        print(f"Data written to CSV for {hostname}")
+            # Write to CSV
+            with open("sfp.csv", mode="a", newline="") as file:
+                writer = csv.writer(file)
+                for sfp_info in match:
+                    writer.writerow([hostname, sfp_info[0], sfp_info[1]])
 
-        # Disconnect from the device
-        net_connect.disconnect()
+            print(f"Data written to CSV for {hostname}")
 
-    except SSHException as e:
-        print(f"SSH Connection Error: {e}")
+            # Disconnect from the device
+            net_connect.disconnect()
 
-    except Exception as ex:
-        print(f"An error occurred: {ex}")
+        except SSHException as e:
+            print(f"SSH Connection Error: {e}")
+
+        except Exception as ex:
+            print(f"An error occurred: {ex}")
