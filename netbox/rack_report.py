bfa855661dac44f2a3641dcf11bcee86b6b83f3e
San Pedro <84334698+pstepanov-crypto@users.noreply.github.com>
2024-12-11 14:44:48 +0300

    Update rack_report.py

--- netbox/rack_report.py
+++ netbox/rack_report.py
@@ -2,7 +2,7 @@ from extras.reports import Report
 from dcim.models import Rack, Device
 
 class RackUnitSpaceReport(Report):
-    description = "Отчет о количестве свободных юнитов в front и rear стойках."
+    description = "Отчет о количестве свободных юнитов в front и rear стойках с учетом устройств full depth."
 
     def test_rack_space(self):
         # Проходим по всем стойкам
@@ -15,12 +15,18 @@ class RackUnitSpaceReport(Report):
             for device in Device.objects.filter(rack=rack, position__isnull=False):
                 # Получаем высоту устройства в юнитах
                 device_height = device.device_type.u_height if device.device_type else 0
+                is_full_depth = device.device_type.is_full_depth if device.device_type else False
 
-                # Проверяем сторону, где установлено устройство, и суммируем его высоту для каждой стороны отдельно
-                if device.face == 'front':
+                # Если устройство full depth, его высота добавляется к обеим сторонам
+                if is_full_depth:
                     occupied_units_front += device_height
-                elif device.face == 'rear':
                     occupied_units_rear += device_height
+                else:
+                    # Проверяем сторону, где установлено устройство, и суммируем его высоту для каждой стороны отдельно
+                    if device.face == 'front':
+                        occupied_units_front += device_height
+                    elif device.face == 'rear':
+                        occupied_units_rear += device_height
 
             # Подсчитываем свободные юниты для передней и задней сторон
             free_units_front = total_units - occupied_units_front
@@ -30,7 +36,7 @@ class RackUnitSpaceReport(Report):
             if free_units_front > 0 or free_units_rear > 0:
                 self.log_success(
                     rack, 
-                    f"Свободных юнитов: {free_units_front} из {total_units},"
+                    f"Свободных юнитов (передняя сторона): {free_units_front} из {total_units}, "
                     f"(задняя сторона): {free_units_rear} из {total_units}."
                 )
             else:
