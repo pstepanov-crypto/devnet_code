2b4bb2612bb08ba25d9f6f6d36e7d867fae46c65
San Pedro <84334698+pstepanov-crypto@users.noreply.github.com>
2025-02-03 14:01:13 +0300

    Create add_user.py

---
+++ add_user/add_user.py
@@ -0,0 +1,55 @@
+from netmiko import ConnectHandler, NetMikoTimeoutException
+import ipaddress
+import yaml
+from paramiko.ssh_exception import SSHException
+from concurrent.futures import ThreadPoolExecutor
+
+# Define the network ranges
+network_ranges = ['192.168.0.0/24', '192.168.1.0/24']
+
+# Define the device parameters
+with open('device_params_ios.yaml') as file:
+    device_params = yaml.load(file, Loader=yaml.FullLoader)
+
+# Define the new user details
+new_user = {
+    'username': 'user1',
+    'privilege': 15,
+    'password': 'qwerty'
+}
+
+def add_user_to_device(ip, device_params, user):
+    device_params['ip'] = ip
+    try:
+        # Connect to the device
+        net_connect = ConnectHandler(**device_params)
+
+        # Send command to add new user
+        config_commands = [
+            f"username {user['username']} privilege {user['privilege']} password {user['password']}"
+        ]
+        output = net_connect.send_config_set(config_commands)
+
+        # Save the configuration (write memory)
+        net_connect.save_config()
+
+        # Determine the hostname
+        hostname = net_connect.find_prompt().strip('#>')
+        print(f"User {user['username']} added to {hostname} ({ip})")
+
+        # Disconnect from the device
+        net_connect.disconnect()
+
+    except (SSHException, NetMikoTimeoutException) as e:
+        print(f"Connection Error to {ip}: {e}")
+    except Exception as ex:
+        print(f"An error occurred on {ip}: {ex}")
+
+# Threads for multiple network ranges
+with ThreadPoolExecutor(max_workers=5) as executor:
+    for network_range in network_ranges:
+        network = ipaddress.ip_network(network_range)
+
+        # Loop through each IP address in the network range
+        for ip in network:
+            executor.submit(add_user_to_device, str(ip), device_params, new_user)
